d64dc76bd25e793f8141ebb26c0ba84c5f6fefb8
kabachok-prog <107309093+kabachok-prog@users.noreply.github.com>
2022-11-18 19:53:39 +0400

    +3 секции

--- js/main.js
+++ js/main.js
@@ -41,4 +41,24 @@ $(function () {
         e.preventDefault()
         $('.testimonials__slider').slick('slickPrev')
     })
+
+    // $('.program__acc-link').on('click', function(e) {
+    //     e.preventDefault()
+    //     $(this).toggleClass('program__acc-link--active')
+    //     $(this).children('.program__acc-text').slideToggle()
+    // }) 
+
+    $('.program__acc-link').on('click', function (e) {
+        e.preventDefault()
+        if ($(this).hasClass('program__acc-link--active')) {
+            $(this).removeClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideUp()
+        } else {
+            $('.program__acc-link').removeClass('program__acc-link--active')
+            $('.program__acc-text').slideUp()
+            $(this).addClass('program__acc-link--active')
+            $(this).children('.program__acc-text').slideDown()
+        }
+    })
+
 })
