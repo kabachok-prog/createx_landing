ed7aaaa2e6cee6d4a0118f5b9c243797184e8e2f
kabachok-prog <107309093+kabachok-prog@users.noreply.github.com>
2022-11-21 21:00:28 +0400

    адаптировал шапку + бургер меню

--- js/main.js
+++ js/main.js
@@ -61,4 +61,27 @@ $(function () {
         }
     })
 
+    // $(window).on('scroll', function(e) {
+    //     if ($(window).scrollTop() > 0) {
+    //         $('.burger').addClass('burger--follow')
+    //     } else {
+    //         $('.burger').removeClass('burger--follow')
+    //     }
+    // })
+
+    setInterval(() =>{
+        if ($(window).scrollTop() > 0 && $('.header__top').hasClass('header__top--open') === false) {
+            $('.burger').addClass('burger--follow')
+        } else {
+            $('.burger').removeClass('burger--follow')
+        }
+    }, 0)
+
+
+    $('.burger, .overlay').on('click', function(e) {
+        e.preventDefault()
+        $('.header__top').toggleClass('header__top--open')
+        $('.overlay').toggleClass('overlay--show')
+    })
+
 })
